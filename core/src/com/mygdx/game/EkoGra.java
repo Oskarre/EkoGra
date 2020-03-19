5a09fadd85fbb0ee0e0b3e3a4b9253e86a41243e
kotar <kotarskiadam65@gmail.com>
2020-03-19 21:43:14 +0100

    SMP-1 not changes it was example message

--- core/src/com/mygdx/game/EkoGra.java
+++ core/src/com/mygdx/game/EkoGra.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class EkoGra extends ApplicationAdapter {
+	// todo project
 	SpriteBatch batch;
 	Texture img;
 	
