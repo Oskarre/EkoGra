aaa4a5135432f923d114a70249842310670d4e82
ClockMMaster <mmwmm@op.pl>
2020-03-24 20:34:45 +0100

    SMP-12 delete useless comments

--- core/src/com/mygdx/game/EkoGra.java
+++ core/src/com/mygdx/game/EkoGra.java
@@ -7,7 +7,6 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class EkoGra extends ApplicationAdapter {
-	// todo project
 	SpriteBatch batch;
 	Texture img;
 	
