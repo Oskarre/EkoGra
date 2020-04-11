8c85c74c324e7bd3c0608b038d9aa33c0b9fe437
Oskarre <oskar98116@gmail.com>
2020-04-11 22:49:34 +0200

    Changing the project to desktop+android and java as main language.

---
+++ desktop/src/com/group/gra/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.group.gra.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.group.gra.EkoGra;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new EkoGra(), config);
+	}
+}
