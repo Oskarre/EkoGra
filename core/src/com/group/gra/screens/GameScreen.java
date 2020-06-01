c5b987051cc1b4b925036d8a6f9e68e26ee7a9ee
kotar <kotarskiadam65@gmail.com>
2020-06-02 00:35:03 +0200

    DEMO added drag and drop event and counter

--- core/src/com/group/gra/screens/GameScreen.java
+++ core/src/com/group/gra/screens/GameScreen.java
@@ -1,6 +1,5 @@
 package com.group.gra.screens;
 
-import com.badlogic.gdx.Audio;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Music;
@@ -10,126 +9,168 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
-import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop;
-import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop.*;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.group.gra.DragAndDropNew;
-import com.group.gra.trashes.Trash;
-import com.group.gra.trashes.TrashGenerator;
+import com.group.gra.trashes.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.group.gra.DragAndDropNew.*;
 
 public class GameScreen implements Screen {
-    public static final int DELAY_TIME = 2;
+    private int DELAY_TIME = 2;
+
     private Stage stage;
     private SpriteBatch sb;
     private Sprite spriteBackground;
-    private Sprite plasticContainer;
-    private Sprite paperContainer;
-    private Sprite hazardousContainer;
-    private Sprite glassContainer;
-    private Sprite bioContainer;
-    private Sprite container;
+    private Image plasticContainer;
+    private Image paperContainer;
+    private Image hazardousContainer;
+    private Image glassContainer;
+    private Image bioContainer;
+    private Image mixedContainer;
     private Sprite maciag;
     private Music backgroundMusic;
-    private Skin skin;
-    private TextureAtlas atlas;
+    private Map<String, String> correctMatches;
+    private Integer counter;
+    private Label label;
 
     public GameScreen(SpriteBatch sb) {
-
         this.sb = sb;
+        FitViewport viewPort = new FitViewport(800, 480);
+        stage = new Stage(viewPort, sb);
+        Gdx.input.setInputProcessor(stage);
+
         spriteBackground = new Sprite(new Texture("gameScreenBackground.png"));
         spriteBackground.setSize(800, 480);
 
-        plasticContainer = new Sprite(new Texture("plasticContainer.png"));
-        plasticContainer.setSize(100, 100);
-        plasticContainer.setPosition(100, 150);
-
-        paperContainer = new Sprite(new Texture("paperContainer.png"));
-        paperContainer.setSize(100, 100);
-        paperContainer.setPosition(200, 150);
-
-        glassContainer = new Sprite(new Texture("glassContainer.png"));
-        glassContainer.setSize(100, 100);
-        glassContainer.setPosition(300, 150);
-
-        hazardousContainer = new Sprite(new Texture("hazardousContainer.png"));
-        hazardousContainer.setSize(100, 100);
-        hazardousContainer.setPosition(400, 150);
-
-        bioContainer = new Sprite(new Texture("bioContainer.png"));
-        bioContainer.setSize(100, 100);
-        bioContainer.setPosition(500, 150);
-
-        container = new Sprite(new Texture("container.png"));
-        container.setSize(100, 100);
-        container.setPosition(600, 150);
         maciag = new Sprite(new Texture("maciag_1.png"));
-        maciag.setSize(1500
-                , 200);
+        maciag.setSize(1500, 200);
         maciag.setPosition(-350, 0);
+        createContainers();
+        correctMatches = new HashMap<>();
+        counter = 0;
+        label = createCounterLabel();
+        stage.addActor(label);
 
+        playMusic(true);
+    }
 
+    private Label createCounterLabel() {
+        TextureAtlas atlas = new TextureAtlas("ui/uiskin.atlas");
+        Skin skin = new Skin(Gdx.files.internal("ui/uiskin.json"), atlas);
+        Label label = new Label(counter.toString(), skin);
+        label.setColor(Color.BLACK);
+        label.setBounds(600, 200, 100, 100);
+        return label;
+    }
+
+    private void createContainers() {
+        plasticContainer = createContainer("plasticContainer.png", 100, 150);
+        paperContainer = createContainer("paperContainer.png", 200, 150);
+        glassContainer = createContainer("glassContainer.png", 300, 150);
+        hazardousContainer = createContainer("hazardousContainer.png", 400, 150);
+        bioContainer = createContainer("bioContainer.png", 500, 150);
+        mixedContainer = createContainer("container.png", 600, 150);
+    }
+
+    private Image createContainer(String fileName, int x, int y) {
+        Image container = new Image(new Texture(fileName));
+        container.setSize(100, 100);
+        container.setPosition(x, y);
+        return container;
+    }
+
+    private void playMusic(boolean turnMusic) {
+        if (turnMusic) {
+            backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("music/gameScreenMusic.mp3"));
+            backgroundMusic.setLooping(true);
+            backgroundMusic.setVolume(0.1f);
+            backgroundMusic.play();
+        }
     }
 
     @Override
     public void show() {
-//        FitViewport viewPort = new FitViewport(800, 480);
-//        stage = new Stage(viewPort, sb);
-//        displayTrashes();
-//        backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("music/gameScreenMusic.mp3"));
-//        backgroundMusic.setLooping(true);
-//        backgroundMusic.setVolume(0.1f);
-//        backgroundMusic.play();
-        FitViewport viewPort = new FitViewport(800, 480);
-        stage = new Stage(viewPort, sb);
-        Gdx.input.setInputProcessor(stage);
-        atlas = new TextureAtlas("ui/uiskin.atlas");
-        skin = new Skin(Gdx.files.internal("ui/uiskin.json"), atlas);
-
         TrashGenerator generator = new TrashGenerator();
-        final Array<Trash> trashArray = generator.generateTrashArray(1);
-        final Image trash = trashArray.get(0).getImage();
-        trash.setBounds(100,100,100,100);
-        final SequenceAction sequence = new SequenceAction(Actions.hide(), Actions.delay(DELAY_TIME), Actions.show(),
-                Actions.moveTo(640, 100, 4f), Actions.hide());
-        trash.addAction(sequence);
-        stage.addActor(trash);
+        Array<Trash> trashArray = generator.generateTrashArray(10);
+        for (Trash trash : trashArray) {
+            System.out.println(trash.getName());
+            correctMatches.put(trash.getName(), "Zle");
+            DragAndDropNew dragAndDrop = new DragAndDropNew();
+            trash.getImage().setPosition(0, 100);
+            SequenceAction sequence = new SequenceAction(Actions.hide(), Actions.delay(DELAY_TIME), Actions.show(),
+                    Actions.moveTo(640, 100, 4f), Actions.removeActor());
 
-        final DragAndDropNew dragAndDrop = new DragAndDropNew();
-        dragAndDrop.addSource(new DragAndDropNew.Source(trash) {
+            addContainersAsActors();
+            initializeDragAndDrop(trash, dragAndDrop, sequence);
+            stage.addActor(trash.getImage());
+            trash.getImage().addAction(sequence);
+            DELAY_TIME += 2;
+        }
+    }
+
+    private void addContainersAsActors() {
+        stage.addActor(plasticContainer);
+        stage.addActor(bioContainer);
+        stage.addActor(glassContainer);
+        stage.addActor(hazardousContainer);
+        stage.addActor(paperContainer);
+        stage.addActor(mixedContainer);
+    }
+
+    private void initializeDragAndDrop(Trash trash, DragAndDropNew dragAndDrop, SequenceAction sequence) {
+        createDragAndDropSource(trash, sequence, dragAndDrop);
+        createDragAndDropTarget(trash.getImage(), dragAndDrop, bioContainer, Bio.class);
+        createDragAndDropTarget(trash.getImage(), dragAndDrop, plasticContainer, Plastic.class);
+        createDragAndDropTarget(trash.getImage(), dragAndDrop, hazardousContainer, HazardousTrash.class);
+        createDragAndDropTarget(trash.getImage(), dragAndDrop, glassContainer, Glass.class);
+        createDragAndDropTarget(trash.getImage(), dragAndDrop, mixedContainer, MixedTrash.class);
+        createDragAndDropTarget(trash.getImage(), dragAndDrop, paperContainer, Paper.class);
+    }
+
+    private void createDragAndDropSource(final Trash trash, final SequenceAction sequence, final DragAndDropNew dragAndDrop) {
+        dragAndDrop.addSource(new Source(trash.getImage()) {
             @Override
-            public DragAndDropNew.Payload dragStart(InputEvent event, float x, float y, int pointer) {
-                DragAndDropNew.Payload payload = new DragAndDropNew.Payload();
-                payload.setObject("Some Paylod!");
+            public Payload dragStart(InputEvent event, float x, float y, int pointer) {
+                Payload payload = new Payload();
+                payload.setObject(trash);
                 payload.setDragActor(getActor());
-                dragAndDrop.setDragActorPosition( getActor().getWidth()/2, -(getActor().getHeight()/2) );
+                dragAndDrop.setDragActorPosition(getActor().getWidth() / 2, -(getActor().getHeight() / 2));
                 getActor().removeAction(sequence);
                 return payload;
             }
+
+            @Override
+            public void dragStop(InputEvent event, float x, float y, int pointer, Payload payload, Target target) {
+                if (target == null)
+                    getActor().remove();
+                //TODO trashes on ground
+            }
         });
-
-
     }
-    private void displayTrashes() {
-        TrashGenerator generator = new TrashGenerator();
-        Array<Trash> trashArray = generator.generateTrashArray(50);
 
-        int x = DELAY_TIME;
-        for (Trash t : trashArray) {
-            Image image = t.getImage();
-            image.setPosition(0, 100);
-            SequenceAction sequence = new SequenceAction(Actions.hide(), Actions.delay(x), Actions.show(),
-                    Actions.moveTo(640, 100, 4f), Actions.hide());
-            image.addAction(sequence);
-            stage.addActor(image);
-            x += DELAY_TIME;
-        }
+    private void createDragAndDropTarget(final Image trash, DragAndDropNew dragAndDrop, Image container, final Object objectType) {
+        dragAndDrop.addTarget(new Target(container) {
+            @Override
+            public boolean drag(Source source, Payload payload, float x, float y, int pointer) {
+                return payload.getObject().getClass().equals(objectType);
+            }
+
+            @Override
+            public void drop(Source source, Payload payload, float x, float y, int pointer) {
+                counter += 1;
+                label.setText(counter.toString());
+                trash.remove();
+            }
+        });
     }
 
     @Override
@@ -139,12 +180,6 @@ public class GameScreen implements Screen {
 
         sb.begin();
         spriteBackground.draw(sb);
-        plasticContainer.draw(sb);
-        paperContainer.draw(sb);
-        bioContainer.draw(sb);
-        glassContainer.draw(sb);
-        hazardousContainer.draw(sb);
-        container.draw(sb);
         maciag.draw(sb);
         sb.end();
         stage.act(delta);
@@ -154,6 +189,7 @@ public class GameScreen implements Screen {
     @Override
     public void resize(int width, int height) {
         stage.getViewport().update(width, height);
+        System.out.println(correctMatches);
     }
 
     @Override
